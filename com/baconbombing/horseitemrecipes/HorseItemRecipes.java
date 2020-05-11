ddef8048916a3e7272c1fbe97d73331e420be01c
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-05-10 19:01:41 -0600

    Add files via upload

--- com/baconbombing/horseitemrecipes/HorseItemRecipes.java
+++ com/baconbombing/horseitemrecipes/HorseItemRecipes.java
@@ -2,10 +2,9 @@ package com.baconbombing.horseitemrecipes;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = HorseItemRecipes.MODID, name = HorseItemRecipes.NAME, version = HorseItemRecipes.VERSION)
+@Mod(modid = HorseItemRecipes.MODID, name = HorseItemRecipes.NAME)
 public class HorseItemRecipes
 {
     public static final String MODID = "horseitemrecipes";
     public static final String NAME = "Horse Item Recipes";
-    public static final String VERSION = "1.0.0";
 }
